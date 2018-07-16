8223f39415ba11c8484d5a7e654e7c8b0065754d
i2143 <i2143@DESKTOP-74BGOQB>
2018-07-16 21:45:37 +0900

    環境構築

--- web/src/main/java/jp/ingenium/web/controller/DefaultController.java
+++ web/src/main/java/jp/ingenium/web/controller/DefaultController.java
@@ -2,13 +2,14 @@ package jp.ingenium.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DefaultController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
-		return "/index.html";
+		return "index";
 	}
 
 }
